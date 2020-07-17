c2d8cd69726e2a1ca394f237a88583a1425405e5
Bruno Muciño <mucinoab@gmail.com>
2020-07-16 21:05:01 -0500

    Procedural mountains with noise.

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,78 @@
 use std::io::{self, Write};
 
 use ggez::event::{self, EventHandler, KeyCode, KeyMods};
-use ggez::graphics::{self, DrawMode};
 use ggez::input::keyboard;
-use ggez::{timer, Context, GameResult};
+use ggez::{graphics, timer, Context, GameResult};
+
+use ggez::graphics::DrawParam;
+use rand::prelude::*;
+use simdnoise::*;
 
 use ggez::nalgebra as na;
-use rand::prelude::*;
 type Point2 = na::Point2<f32>;
 //use cgmath;
 
 struct MainState {
     position_x: f32,
-    stars: Vec<Line>,
+    meshes: Vec<graphics::Mesh>,
 }
 
 impl MainState {
-    fn new(ctx: &Context) -> GameResult<MainState> {
-        let num_stars = 50;
-        let mut stars: Vec<Line> = Vec::with_capacity(num_stars);
-        let mut rng = rand::thread_rng();
-        let (max_x, max_y) = graphics::size(ctx);
+    fn new(ctx: &mut Context) -> GameResult<MainState> {
+        let num_stars = 35;
 
-        for _ in 0..num_stars {
-            let x = rng.gen_range(0.0, max_x);
-            let y = rng.gen_range(0.0, max_y / 2.0);
-            stars.push(Line {
-                points: [x, y, x + 1.0, y + 1.0],
-                width: 1.0,
-                color: graphics::WHITE,
-            })
-        }
+        let meshes = vec![build_stars(ctx, num_stars), build_mountain(ctx)];
 
         let s = MainState {
             position_x: 0.0,
-            stars,
+            meshes,
         };
 
         Ok(s)
     }
 }
 
-struct Line {
-    points: [f32; 4],
-    width: f32,
-    color: graphics::Color,
+fn build_stars(ctx: &mut Context, num_stars: usize) -> graphics::Mesh {
+    let mb = &mut graphics::MeshBuilder::new();
+    let mut rng = rand::thread_rng();
+
+    let (max_x, mut max_y) = graphics::size(ctx);
+    max_y /= 2.0;
+
+    for _ in 0..num_stars {
+        let x = rng.gen_range(0.0, max_x);
+        let y = rng.gen_range(0.0, max_y);
+        let z = rng.gen_range(1.0, 2.0);
+
+        mb.line(
+            &[Point2::new(x, y), Point2::new(x + 0.5, y + 0.5)],
+            z,
+            graphics::WHITE,
+        )
+        .unwrap();
+    }
+
+    mb.build(ctx).unwrap()
+}
+
+fn build_mountain(ctx: &mut Context) -> graphics::Mesh {
+    let mb = &mut graphics::MeshBuilder::new();
+    let (max_x, mut max_y) = graphics::size(ctx);
+    let mut points: Vec<[f32; 2]> = Vec::with_capacity(max_x.ceil() as usize);
+
+    let min_y = max_y / 2.0;
+    max_y = min_y + 100.0;
+
+    let noise = NoiseBuilder::gradient_1d(max_x as _).generate_scaled(min_y, max_y);
+
+    for x in (0..max_x as usize).step_by(10) {
+        points.push([x as f32, noise[x]]);
+    }
+
+    mb.polyline(graphics::DrawMode::stroke(1.0), &points, graphics::WHITE)
+        .unwrap();
+
+    mb.build(ctx).unwrap()
 }
 
 struct _Ship {
@@ -56,12 +84,12 @@ struct _Ship {
 
 impl EventHandler for MainState {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
-        // Increase or decrease `position_x` by 0.5, or by 5.0 if Shift is held.
-        //
         if timer::ticks(ctx) % 100 == 0 {
-            print!("\rAverage FPS: {}", timer::fps(ctx));
+            print!("\rAverage FPS: {:.0}", timer::fps(ctx));
             io::stdout().flush().unwrap();
         }
+
+        // Increase or decrease `position_x` by 0.5, or by 5.0 if Shift is held.
         if keyboard::is_key_pressed(ctx, KeyCode::Right) {
             if keyboard::is_mod_active(ctx, KeyMods::SHIFT) {
                 self.position_x += 4.5;
@@ -79,37 +107,11 @@ impl EventHandler for MainState {
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         graphics::clear(ctx, graphics::BLACK);
 
-        for star in &self.stars {
-            let li = graphics::Mesh::new_line(
-                ctx,
-                &[
-                    [star.points[0], star.points[1]],
-                    [star.points[2], star.points[3]],
-                ],
-                star.width,
-                star.color,
-            )?;
-            graphics::draw(ctx, &li, graphics::DrawParam::default())?;
+        for m in &self.meshes {
+            graphics::draw(ctx, m, DrawParam::default())?;
         }
 
-        let rotation = timer::ticks(ctx) % 1000;
-
-        let circle = graphics::Mesh::new_circle(
-            ctx,
-            DrawMode::stroke(3.0),
-            Point2::new(0.0, 0.0),
-            100.0,
-            5.0,
-            graphics::WHITE,
-        )?;
-
-        graphics::draw(
-            ctx,
-            &circle,
-            (Point2::new(400.0, 300.0), rotation as f32, graphics::WHITE),
-        )?;
-
-        graphics::present(ctx)?;
+        graphics::present(ctx).unwrap();
         timer::yield_now();
         Ok(())
     }
@@ -119,7 +121,7 @@ impl EventHandler for MainState {
             // Quit if Shift+Ctrl+Q is pressed.
             KeyCode::Q => {
                 if mods.contains(KeyMods::SHIFT & KeyMods::CTRL) {
-                    println!("Terminating!");
+                    println!("\nTerminating!");
                     event::quit(ctx);
                 } else if mods.contains(KeyMods::SHIFT) || mods.contains(KeyMods::CTRL) {
                     println!("You need to hold both Shift and Control to quit.");
@@ -135,6 +137,6 @@ impl EventHandler for MainState {
 pub fn main() -> GameResult {
     let cb = ggez::ContextBuilder::new("super_simple", "ggez");
     let (ctx, event_loop) = &mut cb.build()?;
-    let state = &mut MainState::new(&ctx)?;
+    let state = &mut MainState::new(ctx)?;
     event::run(ctx, event_loop, state)
 }
