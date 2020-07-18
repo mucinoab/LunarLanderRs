84ea1519835fdd796f945dad75bc1aa1c48732f7
Bruno Muciño <mucinoab@gmail.com>
2020-07-18 02:04:47 -0500

    Collision system progress.

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,284 @@
+use std::f32::consts::PI;
+
 use ggez::{
     conf,
-    event::{self, EventHandler, KeyCode},
+    event::{self, EventHandler, KeyCode, KeyMods},
     graphics,
     graphics::DrawParam,
-    input::keyboard,
-    timer, Context, GameResult,
+    nalgebra as na, timer, Context, GameResult,
 };
-
+use ncollide2d::{nalgebra as nac, shape};
 use rand::prelude::*;
 use simdnoise::NoiseBuilder;
 
-use ggez::nalgebra;
-type Point2 = nalgebra::Point2<f32>;
-type Vector2 = nalgebra::Vector2<f32>;
+type Point2 = na::Point2<f32>;
+type Vector2 = na::Vector2<f32>;
 
 struct MainState {
     meshes: Vec<graphics::Mesh>,
     font: graphics::Font,
     ship: Ship,
+    input: InputState,
+    _mountain: shape::Polyline<f32>,
 }
 
 impl MainState {
     fn new(ctx: &mut Context) -> GameResult<MainState> {
         let num_stars = 30;
 
-        let meshes = vec![build_stars(ctx, num_stars), build_mountain(ctx)];
+        let (mesh_mountain, mountain) = build_mountain(ctx);
+        let meshes = vec![build_stars(ctx, num_stars), mesh_mountain];
 
         let s = MainState {
             meshes,
             font: graphics::Font::default(),
             ship: Ship::new(ctx),
+            input: InputState::default(),
+            _mountain: mountain,
         };
 
         Ok(s)
     }
+}
+
+struct Ship {
+    pos: Point2,
+    facing: f32,
+    velocity: Vector2,
+    _bbox_size: f32,
+    sprite: graphics::Mesh,
+    armor: shape::Polyline<f32>,
+}
+
+impl Ship {
+    fn new(ctx: &mut Context) -> Ship {
+        let point = Point2::new(100.0, 100.0);
+
+        Ship {
+            pos: point,
+            facing: PI,
+            velocity: na::zero(),
+            _bbox_size: 12.0,
+            armor: shape::Polyline::new(
+                vec![
+                    ncollide2d::nalgebra::Point2::new(0.0, 10.0),
+                    ncollide2d::nalgebra::Point2::new(-10.0, -10.0),
+                    ncollide2d::nalgebra::Point2::new(0.0, -5.0),
+                    ncollide2d::nalgebra::Point2::new(10.0, -10.0),
+                    ncollide2d::nalgebra::Point2::new(0.0, 10.0),
+                ],
+                None,
+            ),
+            sprite: graphics::Mesh::new_polyline(
+                ctx,
+                graphics::DrawMode::stroke(2.0),
+                &[
+                    Point2::new(0.0, 10.0),
+                    Point2::new(-10.0, -10.0),
+                    Point2::new(0.0, -5.0),
+                    Point2::new(10.0, -10.0),
+                    Point2::new(0.0, 10.0),
+                ],
+                graphics::WHITE,
+            )
+            .unwrap(),
+        }
+    }
+}
+
+fn draw_ship(ship: &Ship, ctx: &mut Context) -> GameResult {
+    let drawparams = graphics::DrawParam::new()
+        .dest(ship.pos)
+        .rotation(ship.facing)
+        .offset(Point2::new(0.5, 0.5));
+    graphics::draw(ctx, &ship.sprite, drawparams)
+}
+
+fn vec_from_angle(angle: f32) -> Vector2 {
+    let vx = angle.sin();
+    let vy = angle.cos();
+    Vector2::new(vx, vy)
+}
+
+fn player_thrust(actor: &mut Ship, dt: f32) {
+    let thrust: f32 = 500.0;
+    let direction_vector = vec_from_angle(actor.facing);
+    let thrust_vector = direction_vector * thrust;
+    actor.velocity += thrust_vector * (dt);
+}
+
+fn update_actor_position(actor: &mut Ship, dt: f32) {
+    const MAX_PHYSICS_VEL: f32 = 500.0;
+    let norm_sq = actor.velocity.norm_squared();
+    if norm_sq > MAX_PHYSICS_VEL.powi(2) {
+        actor.velocity = actor.velocity / norm_sq.sqrt() * MAX_PHYSICS_VEL;
+    }
+    let dv = actor.velocity * (dt);
+    actor.pos += dv;
+}
+
+fn player_handle_input(actor: &mut Ship, input: &InputState, dt: f32) {
+    const PLAYER_TURN_RATE: f32 = 1.0;
+    actor.facing += dt * PLAYER_TURN_RATE * input.xaxis;
+
+    if input.yaxis == 1.0 {
+        player_thrust(actor, dt);
+    } else if input.yaxis == -1.0 {
+        player_thrust(actor, -dt);
+    }
+}
+
+fn collisions(ship: &Ship, mountain: &shape::Polyline<f32>) -> bool {
+    let m = nac::Isometry2::new(nac::Vector2::new(100.0, 100.0), nac::zero());
+    let s = nac::Isometry2::new(nac::Vector2::new(ship.pos.x, ship.pos.y), 1.0);
+
+    let n = match ncollide2d::query::contact(&m, mountain, &s, &ship.armor, 1.0) {
+        Some(n) => n.depth,
+        _ => 0.0,
+    };
+
+    if n >= 0.0 {
+        true
+    } else {
+        false
+    }
+}
+
+struct InputState {
+    xaxis: f32,
+    yaxis: f32,
+}
+
+impl Default for InputState {
+    fn default() -> Self {
+        InputState {
+            xaxis: 0.0,
+            yaxis: 0.0,
+        }
+    }
+}
+
+impl EventHandler for MainState {
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        const DESIRED_FPS: u32 = 60;
+        let (sx, sy) = graphics::size(ctx);
+        let seconds = 1.0 / (DESIRED_FPS as f32);
+
+        while timer::check_update_time(ctx, DESIRED_FPS) {
+            player_handle_input(&mut self.ship, &self.input, seconds);
+            update_actor_position(&mut self.ship, seconds);
+
+            if self.ship.pos.x > sx {
+                self.ship.pos.x = 0.0;
+            } else if self.ship.pos.x < 0.0 {
+                self.ship.pos.x = sx;
+            }
+
+            if self.ship.pos.y < 0.0 {
+                self.ship.pos.y = sy;
+            } else if self.ship.pos.y > sy {
+                self.ship.pos.y = 0.0;
+            }
+        }
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        graphics::clear(ctx, graphics::BLACK);
+
+        for m in &self.meshes {
+            graphics::draw(ctx, m, DrawParam::default())?;
+        }
+
+        draw_ship(&self.ship, ctx)?;
+
+        graphics::draw(
+            ctx,
+            &graphics::Text::new((
+                format!(
+                    "FPS{:.0}\nPos({:.0},{:.0})\nFacing{:.4}\nVel({:.2},{:.2}\nCol{})",
+                    timer::fps(ctx),
+                    self.ship.pos.x,
+                    self.ship.pos.x,
+                    self.ship.facing % (2.0 * PI),
+                    self.ship.velocity.x,
+                    self.ship.velocity.y,
+                    collisions(&self.ship, &self._mountain),
+                ),
+                self.font,
+                15.0,
+            )),
+            DrawParam::default(),
+        )?;
+
+        graphics::present(ctx).unwrap();
+        timer::yield_now();
+        Ok(())
+    }
+
+    fn key_down_event(
+        &mut self,
+        ctx: &mut Context,
+        keycode: KeyCode,
+        _keymod: KeyMods,
+        _repeat: bool,
+    ) {
+        match keycode {
+            KeyCode::Up => {
+                self.input.yaxis = 1.0;
+            }
+
+            KeyCode::Down => {
+                self.input.yaxis = -1.0;
+            }
+
+            KeyCode::Left => {
+                self.input.xaxis = 1.0;
+            }
+
+            KeyCode::Right => {
+                self.input.xaxis = -1.0;
+            }
+
+            KeyCode::Space => {
+                self.ship.velocity.x = 0.0;
+                self.ship.velocity.y = 0.0;
+            }
+
+            KeyCode::Escape => event::quit(ctx),
+            _ => (),
+        }
+    }
+
+    fn key_up_event(&mut self, _ctx: &mut Context, keycode: KeyCode, _keymod: KeyMods) {
+        match keycode {
+            KeyCode::Up | KeyCode::Down => {
+                self.input.yaxis = 0.0;
+            }
+
+            KeyCode::Left | KeyCode::Right => {
+                self.input.xaxis = 0.0;
+            }
+            _ => (),
+        }
+    }
+}
+
+pub fn main() -> GameResult {
+    let cb = ggez::ContextBuilder::new("", "")
+        .window_mode(
+            conf::WindowMode::default()
+                .fullscreen_type(conf::FullscreenType::True)
+                .resizable(true)
+                .borderless(true)
+                .dimensions(1366.0, 768.0),
+        )
+        .window_setup(conf::WindowSetup::default().samples(conf::NumSamples::from_u32(8).unwrap()))
+        .backend(conf::Backend::default().version(4, 6));
+
+    let (ctx, event_loop) = &mut cb.build()?;
+    let state = &mut MainState::new(ctx)?;
+    event::run(ctx, event_loop, state)
 }
 
 fn build_stars(ctx: &mut Context, num_stars: usize) -> graphics::Mesh {
@@ -41,7 +286,7 @@ fn build_stars(ctx: &mut Context, num_stars: usize) -> graphics::Mesh {
     let mut rng = rand::thread_rng();
 
     let (max_x, mut max_y) = graphics::size(ctx);
-    max_y /= 2.0;
+    max_y = (max_y / 2.0) + 100.0;
 
     for _ in 0..num_stars {
         let x = rng.gen_range(0.0, max_x);
@@ -59,150 +304,40 @@ fn build_stars(ctx: &mut Context, num_stars: usize) -> graphics::Mesh {
     mb.build(ctx).unwrap()
 }
 
-fn build_mountain(ctx: &mut Context) -> graphics::Mesh {
+fn build_mountain(ctx: &mut Context) -> (graphics::Mesh, shape::Polyline<f32>) {
     let mb = &mut graphics::MeshBuilder::new();
     let (max_x, mut max_y) = graphics::size(ctx);
-    let mut points: Vec<[f32; 2]> = Vec::with_capacity(max_x.ceil() as usize);
 
-    let min_y = max_y / 2.0;
+    let mut points_mesh: Vec<_> = Vec::with_capacity(max_x.ceil() as usize);
+    let mut points_geometry: Vec<_> = Vec::with_capacity(max_x.ceil() as usize);
+
+    let min_y = (max_y / 2.0) + 100.0;
     max_y = min_y + 150.0;
-    let noise = NoiseBuilder::gradient_1d(max_x as _).generate_scaled(min_y, max_y);
+
+    let noise = NoiseBuilder::gradient_1d(max_x as _)
+        .with_seed(rand::random::<i32>())
+        .generate_scaled(min_y, max_y);
 
     for x in (0..max_x as usize).step_by(15) {
-        points.push([x as f32, noise[x]]);
+        points_mesh.push([x as f32, noise[x]]);
+        points_geometry.push(ncollide2d::nalgebra::Point2::new(x as f32, noise[x]));
     }
 
-    mb.polyline(graphics::DrawMode::stroke(1.0), &points, graphics::WHITE)
-        .unwrap();
+    mb.polyline(
+        graphics::DrawMode::stroke(1.0),
+        &points_mesh,
+        graphics::WHITE,
+    )
+    .unwrap();
 
-    mb.build(ctx).unwrap()
+    (
+        mb.build(ctx).unwrap(),
+        shape::Polyline::new(points_geometry, None),
+    )
 }
 
-struct _Gui {
-    score: i32,
-    time: f32,
-    altitude: f32,
-    fuel: i32,
-    horizontal_speed: f32,
-    vertical_speed: f32,
-}
-
-struct Ship {
-    pos: Point2,
-    facing: f32,
-    velocity: Vector2,
-    ang_vel: f32,
-    bbox_size: f32,
-    sprite: graphics::Mesh,
-}
-
-impl Ship {
-    fn new(ctx: &mut Context) -> Ship {
-        Ship {
-            pos: Point2::new(100.0, 100.0),
-            facing: 0.0,
-            velocity: nalgebra::zero(),
-            ang_vel: 0.0,
-            bbox_size: 12.0,
-            sprite: graphics::Mesh::new_rectangle(
-                ctx,
-                graphics::DrawMode::fill(),
-                graphics::Rect::new(Point2::origin()[0], Point2::origin()[1], 10.0, 10.0),
-                graphics::WHITE,
-            )
-            .unwrap(),
-        }
-    }
-}
-
-fn draw_ship(ship: &Ship, ctx: &mut Context) -> GameResult {
-    let drawparams = graphics::DrawParam::new()
-        .dest(Point2::new(ship.pos[0], ship.pos[1]))
-        .rotation(ship.facing);
-    graphics::draw(ctx, &ship.sprite, drawparams)
-}
-
-fn wrap_actor_position(actor: &mut Ship, ctx: &Context) {
-    let (sx, sy) = graphics::size(ctx);
-    // Wrap screen
-    let screen_x_bounds = sx / 2.0;
-    let screen_y_bounds = sy / 2.0;
-    if actor.pos.x > screen_x_bounds {
-        actor.pos.x -= sx;
-    } else if actor.pos.x < -screen_x_bounds {
-        actor.pos.x += sx;
-    };
-    if actor.pos.y > screen_y_bounds {
-        actor.pos.y -= sy;
-    } else if actor.pos.y < -screen_y_bounds {
-        actor.pos.y += sy;
-    }
-}
-
-fn _world_to_screen_coords(screen_width: f32, screen_height: f32, point: Point2) -> Point2 {
-    let x = point.x + screen_width / 2.0;
-    let y = screen_height - (point.y + screen_height / 2.0);
-    Point2::new(x, y)
-}
-
-impl EventHandler for MainState {
-    fn update(&mut self, ctx: &mut Context) -> GameResult {
-        if keyboard::is_key_pressed(ctx, KeyCode::Right) {
-            self.ship.pos[0] += 3.0;
-        }
-        if keyboard::is_key_pressed(ctx, KeyCode::Left) {
-            self.ship.pos[0] -= 3.0;
-        }
-        if keyboard::is_key_pressed(ctx, KeyCode::Up) {
-            self.ship.pos[1] -= 3.0;
-        }
-        if keyboard::is_key_pressed(ctx, KeyCode::Down) {
-            self.ship.pos[1] += 3.0;
-        }
-        if keyboard::is_key_pressed(ctx, KeyCode::Space) {
-            self.ship.facing -= 0.1;
-        }
-
-        wrap_actor_position(&mut self.ship, ctx);
-        self.ship.pos[0] += 0.2;
-        self.ship.pos[1] += 0.1;
-
-        Ok(())
-    }
-
-    fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        graphics::clear(ctx, graphics::BLACK);
-
-        for m in &self.meshes {
-            graphics::draw(ctx, m, DrawParam::default())?;
-        }
-
-        draw_ship(&self.ship, ctx)?;
-
-        graphics::draw(
-            ctx,
-            &graphics::Text::new((format!("{:.0}", timer::fps(ctx)), self.font, 35.0)),
-            DrawParam::default(),
-        )?;
-
-        graphics::present(ctx).unwrap();
-        timer::yield_now();
-        Ok(())
-    }
-}
-
-pub fn main() -> GameResult {
-    let cb = ggez::ContextBuilder::new("", "")
-        .window_mode(
-            conf::WindowMode::default()
-                .fullscreen_type(conf::FullscreenType::True)
-                .resizable(true)
-                .borderless(true),
-        )
-        .window_setup(conf::WindowSetup::default().samples(conf::NumSamples::from_u32(8).unwrap()))
-        .backend(conf::Backend::default().version(4, 6));
-
-    let (ctx, event_loop) = &mut cb.build()?;
-    let state = &mut MainState::new(ctx)?;
-    event::run(ctx, event_loop, state)
-}
+//To_Do
+//Particulas?
+//Colisiones, nave, linea
+//movimiento
+//numero de samples
